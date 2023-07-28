2b39fea91560227c9ab18967f0e9c89c4e4eece8
yamasanqb <hayama.hika@gmail.com>
2023-07-29 02:56:12 +0900

    added get_search_book() -- search from isbn

--- src/openapi_server/controllers/search_controller.py
+++ src/openapi_server/controllers/search_controller.py
@@ -4,11 +4,18 @@ from typing import Dict
 from typing import Tuple
 from typing import Union
 
+from openapi_server.models.api_response import ApiResponse
+from openapi_server.models.auth_login_request import AuthLoginRequest
 from openapi_server.models.post_book_request import PostBookRequest  # noqa: E501
 from openapi_server import util
 
+from openapi_server.models.post_book_request import PostBookRequest
 
-def get_search_book(title=None, isbn=None):  # noqa: E501
+from db.models import Session, DBBook
+from sqlalchemy.sql import exists
+
+
+def get_search_book(title: str=None, isbn: str=None):  # noqa: E501
     """search book
 
     search book # noqa: E501
@@ -20,4 +27,19 @@ def get_search_book(title=None, isbn=None):  # noqa: E501
 
     :rtype: Union[PostBookRequest, Tuple[PostBookRequest, int], Tuple[PostBookRequest, int, Dict[str, str]]
     """
-    return 'do some magic!'
+
+    # search book from isbn
+
+    # create session
+    session = Session()
+
+    # check if book which has that isbn exists...
+    if session.query(exists().where(DBBook.isbn == isbn)).scalar() > 0:
+        # print("本あったよ")
+        books = session.query(DBBook.query.filter(DBBook.isbn == isbn)).all()
+        for book in books:
+            # return PostBookRequest with the isbn, title, and author
+            return (PostBookRequest(isbn=isbn, title=book.title, author=book.author), 200)
+    else:
+        # print("本なかったよ泣")
+        return (PostBookRequest(isbn=None, title=None, author=None), 500)
