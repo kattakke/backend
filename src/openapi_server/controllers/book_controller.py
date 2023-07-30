13bce5626686d82bbe75e5e85b83d14419ffb806
yamasanqb <hikaru.y.sk@gmail.com>
2023-07-30 18:54:38 +0900

    updated post_book

--- src/openapi_server/controllers/book_controller.py
+++ src/openapi_server/controllers/book_controller.py
@@ -9,6 +9,9 @@ from openapi_server.models.book import Book  # noqa: E501
 from openapi_server.models.post_book_request import PostBookRequest  # noqa: E501
 from openapi_server import util
 
+from db.models import DBBook, Session
+from sqlalchemy.sql import exists
+
 
 def delete_book_info(book_id):  # noqa: E501
     """delete book info
@@ -53,7 +56,7 @@ def patch_book_info(book_id, post_book_request=None):  # noqa: E501
     return 'do some magic!'
 
 
-def post_book(post_book_request=None):  # noqa: E501
+def post_book(post_book_request: PostBookRequest=None):  # noqa: E501
     """post book info
 
     post book info # noqa: E501
@@ -63,6 +66,30 @@ def post_book(post_book_request=None):  # noqa: E501
 
     :rtype: Union[Book, Tuple[Book, int], Tuple[Book, int, Dict[str, str]]
     """
-    if connexion.request.is_json:
-        post_book_request = PostBookRequest.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+
+    # requires PostBookRequest with title, author, isbn, 
+    # and adds the book to the db. (planning)
+
+    book = {}
+    book['title'] = post_book_request.title()
+    book['author'] = post_book_request.author()
+    book['isbn'] = post_book_request.isbn()
+
+    session = Session()
+
+    if session.query(exists().where(DBBook.isbn == post_book_request.isbn())).scalar() > 0:
+        return (book, 500)
+    else:
+        dbbook = DBBook()
+        dbbook.title = book['title']
+        dbbook.author = book['author']
+        dbbook.isbn = book['isbn']
+
+        session.add(dbbook)
+        session.commit()
+
+    return (book, 200)
+
+    # if connexion.request.is_json:
+    #     post_book_request = PostBookRequest.from_dict(connexion.request.get_json())  # noqa: E501
+    # return 'do some magic!'
